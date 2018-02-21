7876ffab6a037b2b38377c67ad613a3c69935679
Ntiyiso Rikhotso <truminnd@gmail.com>
2018-02-21 12:39:01 +0200

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class React extends CI_Controller {
 	function __construct(){
 		parent::__construct();
+		$this->load->library('reactjs');
 	}
 	public function index(){
 		$this->load->view('index');
